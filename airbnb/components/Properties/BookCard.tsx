e4c7a2834573dd2d7a8bb8c773efa10a38aca72f
Amal Kallarakkal <amalkallarakkal10879@gmail.com>
2025-01-03 16:14:46 +0530

    bookcard complete

--- airbnb/components/Properties/BookCard.tsx
+++ airbnb/components/Properties/BookCard.tsx
@@ -9,23 +9,23 @@ export const BookCard = () => {
     return (
         <Card className="p-6">
             <div className="text-2xl font-semibold flex space-x-3 items-end text-darkText">
-                <div className="line-through text-lightText">$9,972</div>
-                <div>$7,972</div>
+                <div className="line-through text-lightText">₹7,000</div>
+                <div>₹6,000</div>
                 <div className="text-sm pb-0.5">night</div>
             </div>
             <div className="my-5 w-full rounded-md border border-lightText">
                 <div className="w-full flex">
                     <InputwithLabel 
-                        label="Check in" 
+                        label="CHECK-IN" 
                         placeholder="Add dates"
-                        className="rounded-sm"
+                        className="rounded-sm font-semibold"
                         width="50%"
                     />
                     <Separator orientation="vertical" className="w-[1px] bg-lightText"/>
                     <InputwithLabel 
-                        label="Check out" 
+                        label="CHECKOUT" 
                         placeholder="Add dates"
-                        className="rounded-md"
+                        className="rounded-md font-semibold"
                         width="50%"
                     />
                 </div>
@@ -33,13 +33,31 @@ export const BookCard = () => {
                 <InputwithLabel 
                     label="Guests" 
                     placeholder="1 guest"
-                    className="rounded-md"
+                    className="rounded-md font-semibold"
                     width="100%"
                 />
             </div>
             <Button className="w-full rounded-sm font-semibold bg-airbnb hover:bg-airbnbDark text-white hover:text-white">
                 Reserve
             </Button>
+            <div className="text-sm text-lightText text-center p-3">You won't be charged yet</div>
+            <div className="flex justify-between p-2 text-mediumText ">
+                <div className="underline md">₹6,000 x 1 night</div>
+                <div>₹5,000 </div>
+            </div>
+            <div className="flex justify-between p-2 text-mediumText ">
+                <div className="underline md">Special offer</div>
+                <div className="text-green-500">-₹1,000</div>
+            </div>
+            <div className="flex justify-between p-2 text-mediumText ">
+                <div className="underline md">Airbnb service fee</div>
+                <div >₹5,00 </div>
+            </div>
+            <Separator className="h-[0.5px] bg-lightText my-2"/>
+            <div className="flex justify-between p-2 text-darkText font-semibold mt-5">
+                <div className="">Total before Taxes</div>
+                <div>₹5,500 </div>
+            </div>
         </Card>
     );
 }
