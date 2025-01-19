fc2eefde2b9f828efe0012bededad7feea970454
harshitgoyal101 <harshitgoyal101@gmail.com>
2025-01-19 12:26:54 +0530

    get

--- airbnb/components/Properties/Properties.tsx
+++ airbnb/components/Properties/Properties.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { apiService } from "@/app/services/apiService";
 import { PropertyCard } from "./PropertyCard";
 import { useEffect, useState } from "react";
 
@@ -19,17 +20,8 @@ export const Properties = ({
     const [properties, setProperties] = useState<PropertyType[]>([]);
 
     const getProperties = async () => {
-        const url = 'http://localhost:8000/api/properties/';
-        await fetch(url, {
-            method: 'GET',
-        }).then(response => response.json())
-        .then((json) => {
-            console.log('json', json);
-            setProperties(json.data);
-        })
-        .catch((error) => {
-            console.log('error', error);
-        })
+        const tmpProperties = await apiService.get('/api/properties/');
+        setProperties(tmpProperties.data);
     }
 
     useEffect(() => {
