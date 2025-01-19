94c75025906c917fe6af387319e9c454bb48f0aa
harshitgoyal101 <harshitgoyal101@gmail.com>
2025-01-19 11:30:39 +0530

    PropertyCard

--- airbnb/components/Properties/Properties.tsx
+++ airbnb/components/Properties/Properties.tsx
@@ -1,13 +1,22 @@
 "use client"
-import { error } from "console";
+
 import { PropertyCard } from "./PropertyCard";
 import { useEffect, useState } from "react";
+
+export type PropertyType = {
+    id: string,
+    title: string,
+    price_per_night: number,
+    image_url: string
+}
 
 export const Properties = ({
     withTax
 }: {
     withTax: boolean
 }) => {
+
+    const [properties, setProperties] = useState<PropertyType[]>([]);
 
     const getProperties = async () => {
         const url = 'http://localhost:8000/api/properties/';
@@ -16,6 +25,7 @@ export const Properties = ({
         }).then(response => response.json())
         .then((json) => {
             console.log('json', json);
+            setProperties(json.data);
         })
         .catch((error) => {
             console.log('error', error);
@@ -28,7 +38,12 @@ export const Properties = ({
 
     return (
         <div className="w-full px-24 py-3 space-x-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            <PropertyCard/>
+            {properties.map((property) => {
+                return <PropertyCard 
+                    key={property.id}
+                    property={property}
+                />
+            })}
        </div>
     );
 }
--- airbnb/components/Properties/PropertyCard.tsx
+++ airbnb/components/Properties/PropertyCard.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
 import { FaRegHeart } from "react-icons/fa";
+import { PropertyType } from "./Properties";
 
-export const PropertyCard = () => {
+interface PropertyProps {
+    property: PropertyType
+}
+
+export const PropertyCard: React.FC<PropertyProps> = ({
+    property
+}) => {
     return (
         <div className="cursor-pointer m-3 rounded-xl">
             <div>
@@ -11,8 +18,8 @@ export const PropertyCard = () => {
                             fill
                             sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
                             className="hover:scale-R110 object-cover transition h-full w-full"
-                            src="/temp.avif" 
-                            alt="/temp.avif"
+                            src={property.image_url}
+                            alt="Image not found"
                         />
                     </div>
                     <div className="absolute top-4 right-4 text-white text-2xl z-10 cursor-pointer">
@@ -21,7 +28,7 @@ export const PropertyCard = () => {
                 </div>
                 <div className="mt-4 flex flex-col space-x-0 text-lightText">
                     <div className="text-md flex justify-between font-semibold m-0 p-0 text-darkText">
-                        <div>Harshit Goyal</div>
+                        <div>{property.title}</div>
                         <div>★ 4.3</div>
                     </div>
                     <div className="text-sm m-0 p-0">
@@ -31,7 +38,7 @@ export const PropertyCard = () => {
                         123.213
                     </div>
                     <div className="text-sm mt-2 p-0 text-darkText">
-                    <strong>$ 500</strong> per night
+                    <strong>$ {property.price_per_night}</strong> per night
                     </div>
                 </div>
             </div>
