c5a3f75002ec899486bedecaa4c12c054925d947
Amal Kallarakkal <amalkallarakkal10879@gmail.com>
2025-02-21 17:26:43 +0530

    added a new page called EditProfile and its button in userPage

--- airbnb/app/user/[id]/page.tsx
+++ airbnb/app/user/[id]/page.tsx
@@ -15,6 +15,7 @@ import {
 
 import Link from "next/link";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function UserDetailPage() {
     return (
@@ -38,8 +39,14 @@ export default function UserDetailPage() {
                     <Link href="/" className="underline font-semibold">Report this profile</Link>
                 </div>
             </div>
-            <div className="overflow-y-hidden">
+            <div className="overflow-y-hidden">                
                 <div className="min-w-[480px] max-w-[500px] lg:max-w-[1000px] w-full p-10 xl:grid grid-cols-2">
+                    <Link href={'/EditProfile'}>
+                        <Button variant={"active"} size={"lg"} className="w-[160px] mb-4 text-md rounded-md font-semibold bg-airbnb hover:bg-airbnbDark
+                        text-white hover:text-white">
+                            Edit profile
+                        </Button>
+                    </Link>
                     <div className="col-span-2 text-3xl text-darkText font-bold p-2">About Harshit</div>
                        
                     <div className="col-span-2 mt-5 p-2">Zostel is India's first and largest chain of budget hostels for the sociable explorer.
