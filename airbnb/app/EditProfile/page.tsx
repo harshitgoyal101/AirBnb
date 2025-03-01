0202488cea426d3ff15566f1585a8f5f28c0a93b
Amal Kallarakkal <amalkallarakkal10879@gmail.com>
2025-03-01 17:19:38 +0530

    changed amenities  to svg files

--- airbnb/app/EditProfile/page.tsx
+++ airbnb/app/EditProfile/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { IconWithLabel } from '@/components/ui/IconWIthLabel';
+import Image from "next/image";
 import { BsFillCameraFill } from "react-icons/bs";
 export function Avatar({name, size= "small"} : {name: string, size?: string}) {
   return <div className={`relative inline-flex items-center justify-center
