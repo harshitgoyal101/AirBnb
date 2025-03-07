854ef9b434f9025be74ba0697eda61bd5fd46343
Amal Kallarakkal <amalkallarakkal10879@gmail.com>
2025-03-08 02:48:57 +0530

    changes mate to make pages without the global layout

---
+++ airbnb/app/(NotUsongGLobalLayout)/become-a-host/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BecomeHost = () => {
+  return (
+    <div>
+      this will return page weith carousel for inpt
+    </div>
+  )
+}
+
+export default BecomeHost ;
---
+++ airbnb/app/(UsingGlobalLayout)/page.tsx
@@ -0,0 +1,12 @@
+
+import { Categories } from "@/components/Categories";
+import { Properties } from "@/components/Properties/Properties";
+
+export default function Home() {
+	return (
+		<main className="flex flex-col w-full">
+			<Categories />
+			<Properties />
+		</main>
+	);
+}
