27aca08035d595110162bc4bbe20950565317b8e
Amal Kallarakkal <amalkallarakkal10879@gmail.com>
2025-03-13 19:06:58 +0530

    BecomeHost CSS completed need to make it functional

--- airbnb/app/becomehost/page.tsx
+++ airbnb/app/becomehost/page.tsx
@@ -1,3 +1,6 @@
+import { AddDescription } from "@/components/BecomHost/AddDescription";
+import { AddPhotos } from "@/components/BecomHost/AddPhotos";
+import { AmenitiesSelect } from "@/components/BecomHost/AmenitiesSelect";
 import { CategorySelect } from "@/components/BecomHost/CategorySelect";
 import { PlaceInfo } from "@/components/BecomHost/PlaceInfo";
 import { PlaceSelect } from "@/components/BecomHost/PlaceSelect";
@@ -11,18 +14,20 @@ import {
   
 } from "@/components/ui/carousel"
 import  Image  from "next/image";
+import Link from "next/link";
 
 const BecomeHost = () => {
   return (
     <div>
-      <nav className ="fixed top-0 w-full h-24 z-10 py-0 px-16 bg-white mb-16 border-b flex justify-between items-center">
-        
+      <nav className ="fixed top-0 w-full h-24 z-10 py-0 px-16 bg-white border-b flex justify-between items-center">
+        <Link href={"/"}>
           <Image 
             width={32}
             height={32}
             src="/AirBnbBlack.svg"
             alt="Small Image 1"
           />
+        </Link>
           <div>
             <Button size = {"sm"} className="outline outline-1 text-darkText font-semibold mr-4">Questions?</Button>
             <Button size = {"sm"} className="outline outline-1 text-darkText font-semibold">Save & exit</Button>
@@ -37,7 +42,7 @@ const BecomeHost = () => {
         >
                 
         <CarouselContent className="w-full mb-10">
-            <CarouselItem className="mb-24">
+            <CarouselItem className="mb-24 max-h-[475px] overflow-y-auto">
                 <CategorySelect/>
             </CarouselItem>
             
@@ -50,17 +55,21 @@ const BecomeHost = () => {
             </CarouselItem>
 
             <CarouselItem >                
-              <div>amenities</div>
-              <div>type of guests</div>
-              <div>property images</div>
+              <AmenitiesSelect/>
             </CarouselItem>
             
+            <CarouselItem >                
+              <AddPhotos/>
+            </CarouselItem>
                 
-            
+            <CarouselItem >                
+              <AddDescription/>
+            </CarouselItem>
+
         </CarouselContent>   
         <div className ="fixed bottom-0 w-full h-24 px-16 bg-white border-black border-t-4  flex justify-between items-center">
             <Button size = {"lg"} className="underline text-darkText font-semibold mr-4 z-10">Back <CarouselPrevious className="opacity-0 hover:opacity-0 disabled:opacity-0" size={"lg"}/> </Button>
-            <Button size = {"lg"} className="outline outline-1 text-white bg-black rounded-md font-semibold">Next <CarouselNext size={"lg"} className="opacity-0 hover:opacity-0 disabled:opacity-0"/></Button>
+            <Button size = {"lg"} className="outline outline-1 text-white bg-gray-800 hover:bg-black hover:text-white rounded-md font-semibold">Next <CarouselNext size={"lg"} className="opacity-0 hover:opacity-0 disabled:opacity-0"/></Button>
         </div>          
     </Carousel>
     </div>
--- airbnb/components/BecomHost/AmenitiesSelect.tsx
+++ airbnb/components/BecomHost/AmenitiesSelect.tsx
@@ -1,8 +1,39 @@
+"use client"
+import { apiService } from "@/app/services/apiService";
+import { useState, useEffect } from "react";
+import { IconWithLabel } from "../ui/IconWIthLabel";
 
 export const AmenitiesSelect = () => {
+  const [amenities, setAmenities] = useState<string[]>([]);
+      const [loading, setLoading] = useState(true);
+  
+      useEffect(() => {
+          const fetchAmenities = async () => {
+              try {
+                  const response = await apiService.get("/api/aminities/");
+                  setAmenities(response?.data);
+              } catch (error) {
+                  console.error("Error fetching amenities:", error);
+              } finally {
+                  setLoading(false);
+              }
+          };
+  
+          fetchAmenities();
+      }, []);
   return (
-    <div>
-      
+    <div className='px-80 flex-row items-center'> 
+        <div>
+          <p className='mx-20 font-semibold text-3xl'>Tell guests what your place has to offer</p>
+          <p className="mx-20 text-lightText text-lg text-start">You can add more amenities after you publish your listing.</p>
+        </div>
+        <div className='grid grid-cols-4 justify-evenly items-center px-16 my-4 h-fit'>
+            {amenities.map((amenity, index) => {
+                return <div key={index} className="border-2 py-4 px-0  hover:border-black hover:bg-gray-100 max-w-40 rounded-md my-4">
+                    <IconWithLabel className="border-none text-sm w-full" type={String(amenity)}/>
+                </div>
+            })}
+        </div>
     </div>
   )
 }
