116ded88dbc287a97462c2d7632860169f0e3196
Amal Kallarakkal <amalkallarakkal10879@gmail.com>
2025-03-13 19:19:58 +0530

    added input box for property name in AddDescrption

--- airbnb/components/BecomHost/AddDescription.tsx
+++ airbnb/components/BecomHost/AddDescription.tsx
@@ -2,15 +2,26 @@ import { Input } from "../ui/input"
 
 export const AddDescription = () => {
   return (
-    <div className="mx-72 mt-10">
-      <div className="mx-40 flex justify-between">
+    <div className="mx-72 mt-4">
+        <div className="mx-40 flex justify-between">
+        <div>
+          <p className='font-semibold text-3xl'>Name of your property</p>
+        </div>        
+      </div>
+      <div className="my-4 mx-40  h-10 ">
+        <Input 
+            placeholder = "Add your Property's name..." 
+            className="my-2 h-full text-lightText rounded-xl border-black border-2">
+        </Input> 
+      </div>  
+
+      <div className="mx-40 mt-10 flex justify-between">
         <div>
           <p className='font-semibold text-3xl'>Create your description</p>
           <p className="text-lightText text-lg text-start">Share what makes your place special.</p>
-        </div>
-        
+        </div>        
       </div>
-      <div className="my-4 mx-40  h-72 ">
+      <div className="my-4 mx-40 h-56 ">
         <Input 
             placeholder = "Add your decription here..." 
             className="my-2 h-full text-lightText rounded-xl border-black border-2">
--- airbnb/components/BecomHost/AddPhotos.tsx
+++ airbnb/components/BecomHost/AddPhotos.tsx
@@ -6,7 +6,7 @@ export const AddPhotos = () => {
     <div className="mx-72 mt-10">
       <div className="mx-40 flex justify-between">
         <div>
-          <p className='font-semibold text-3xl'>Choose at least 5 photos</p>
+          <p className='font-semibold text-3xl'>Choose at least 4 photos</p>
           <p className="text-lightText text-lg text-start">Drag to reorder</p>
         </div>
         <Button size = "icon" className="bg-gray-100 hover:bg-gray-200">
