41436d3a47ca384f4b7af5812ae6c1387eeb1836
Amal Kallarakkal <amalkallarakkal10879@gmail.com>
2025-03-19 14:19:44 +0530

    simplyfied adding review but FillReview not deleted

--- airbnb/components/Properties/ReviewSection.tsx
+++ airbnb/components/Properties/ReviewSection.tsx
@@ -7,7 +7,7 @@ import { Model } from '@/components/ui/Model'
 import { RatingsSection } from "./RatingsSection";
 import { Input } from '../ui/input';
 import { Separator } from "../ui/separator";
-import Link from "next/link";
+import Rating from '@mui/material/Rating';
 
 export const ReviewSection = () => {
     const [isOpen, setIsOpen] = useState(false);    
@@ -52,11 +52,23 @@ export const ReviewSection = () => {
             <Button onClick={()=>{setIsOpen(!isOpen)}} className="text-darkText outline-gray-800 outline-1 font-semibold rounded-lg" size={"lg"} variant={"outline"}>
                 Show all 9 review
             </Button> 
-            <Link href = {"/FillReview"}>
-                <Button className="text-white mx-4 mt-2 border-2 border-transparent hover:border-black bg-airbnb outline-gray-800 outline-1 font-semibold rounded-lg" size={"lg"} variant={"outline"}>
-                    Add a review 
-                </Button>  
-            </Link>
+            <div className="my-10">
+                
+                <div className="my-3">
+                    <div className="text-2xl font-semibold my-2">Add Overall Rating</div>
+                    <div className="text-lg my-1 text-lightText">How was your overall experience in your stay ?</div>
+                    <Rating name="size-large" size="large" />
+                </div>
+                <div className="my-3 flex flex-col md:flex-row justify-between items-center md:items-end">
+                    <div className="w-full md:w-2/3">
+                        <p className="text-2xl font-semibold">Describe your stay breifly</p>
+                        <Input type="text" className="mt-3 w-full outline outline-2 outline-black h-32"></Input>
+                    </div>
+                    <Button className="text-white mr-4 mt-4 xl:mr-24 border-2 border-transparent hover:border-black bg-airbnb outline-gray-800 outline-1 font-semibold rounded-lg" size={"lg"} variant={"outline"}>
+                        Submit
+                    </Button>  
+                </div>
+            </div>
             <Model
                 isOpen = {isOpen}
                 close   ={()=>{setIsOpen(!isOpen)}}
