625c19ea0b6fcd5d889686b5f63df6f4a328665e
Amal Kallarakkal <amalkallarakkal10879@gmail.com>
2025-03-19 21:49:29 +0530

    added skeletons on categories, properties and becomeHost

--- airbnb/components/BecomHost/CategorySelect.tsx
+++ airbnb/components/BecomHost/CategorySelect.tsx
@@ -2,10 +2,11 @@
 import { useState, useEffect } from "react";
 import { apiService } from "@/app/services/apiService";
 import { IconWithLabel } from "../ui/IconWIthLabel";
+import { Skeleton } from "../ui/skeleton";
 
 export const CategorySelect = () => {
     const [categories, setCategories] = useState<String[]>([]);
-    
+    const [loading, setLoading] = useState(true)
         const getCategories = async () => {
             const tmpCategories = await apiService.get('/api/categories/');
             setCategories(tmpCategories.data);
@@ -13,16 +14,27 @@ export const CategorySelect = () => {
     
         useEffect(() => {
             getCategories();
+            setLoading(false)
         }, [])
   return (
     <div className='px-6 md:px-32 lg:px-40 xl:px-72 flex-row text-center items-center'> 
         <div className='font-semibold text-3xl'>Which of these best describes your place?</div>
         <div className='grid grid-cols-3 md:grid-cols-4 justify-evenly items-center md:px-4 lg:px-8 xl:px-16'>
-            {categories.map((category, index) => {
-                return <div key={index} className="border-2 py-1 mx-1 md:py-2 xl:py-4 px-0 h-32 items-center hover:border-black hover:bg-gray-100 max-w-40 rounded-md my-4">
-                    <IconWithLabel className="border-none text-sm w-full" type={String(category)}/>
-                </div>
-            })}
+            {loading?
+                <>
+                    {Array.from({length: 12}).map((_, index) => (
+                        <div key={index} >
+                            <Skeleton className="py-1 mx-1 w-auto max-w-40 md:py-2 xl:py-4 px-0 h-32 rounded-md my-4"/>     
+                        </div>
+                    ))}
+                </>
+            :            
+                categories.map((category, index) => {
+                    return <div key={index} className="border-2 py-1 mx-1 md:py-2 xl:py-4 px-0 h-32 items-center hover:border-black hover:bg-gray-100 max-w-40 rounded-md my-4">
+                        <IconWithLabel className="border-none text-sm w-full" type={String(category)}/>
+                    </div>
+                })
+            }
         </div>
     </div>
   )
--- airbnb/components/Categories.tsx
+++ airbnb/components/Categories.tsx
@@ -13,16 +13,20 @@ import { RiSoundModuleLine } from "react-icons/ri";
 import { Switch } from "@/components/ui/switch"
 import { IconWithLabel } from "./ui/IconWIthLabel";
 import { useState, useEffect } from 'react';
+import { Skeleton } from "./ui/skeleton";
 
 export const Categories = () => {
 
 	const [withTax, setWithTax] = useState(true);
+
+    const [loading, setLoading] = useState(true)
 
     const [categories, setCategories] = useState<String[]>([]);
 
     const getCategories = async () => {
         const tmpCategories = await apiService.get('/api/categories/');
         setCategories(tmpCategories.data);
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -30,37 +34,63 @@ export const Categories = () => {
     }, [])
     
     return (
-        <div className="w-full mx-auto px-12 md:px-24 lg:px-24 py-3 flex-col lg:flex-row flex space-x-8 items-center justify-end">
-            <Carousel className="w-full md:overflow-auto relative">
-                <CarouselPrevious variant="outline" className="absolute left-1 top-1/2 transform -translate-y-1/2 "/>    
-                <div className="mx-12">
-                    <CarouselContent>
-                        {categories.map((category, index) => {
-                            return <CarouselItem key={index} className="basis-1/10 mt-2">
-                                <IconWithLabel type={String(category)}/>
-                            </CarouselItem>
-                        })}
-                    </CarouselContent>
-                </div>
-                <CarouselNext variant="outline" className="absolute right-1 top-1/2 transform -translate-y-1/2 "/>
-            </Carousel>            
-            <div className="md:space-x-4 mt-2 lg:mt-0 hidden sm:flex">
-                <Button variant={"outline"} className="rounded-lg">
-                    <RiSoundModuleLine className="rotate-90"/>
-                    <div className="text-mediumText font-sm py-2">
-                        Filter
-                    </div>
-                </Button>
+        <div className="w-full mx-auto px-12 md:px-24 lg:px-24 py-3 flex-col lg:flex-row flex space-x-8 items-center ">
+            
+            {loading ?
+                <>
+                    <div className="flex  justify-center items-center">
+                        <div className="flex items-center w-full">
+                            <Skeleton className="h-10 w-10 rounded-full"/>
+                            {Array.from({ length: 8 }).map((_, index) => (
+                                <div key={index} className="cursor-pointer m-3 flex flex-col justify-center items-center rounded-xl" >
+                                    <Skeleton className="w-6 h-6 rounded-sm mb-1"/>                            
+                                    <Skeleton className="w-16 h-4"/>
+                                </div>
+                            ))}
+                            <Skeleton className="h-10 w-10 rounded-full"/>
+                        </div>
 
-                <Button asChild variant={"outline"} className="rounded-lg" onClick={() => {
-                    setWithTax(!withTax)
-                }}> 
-                    <div className="text-mediumText font-sm py-2">              
-                        Display total before taxes
-                        <Switch checked={withTax}/>
+                        <Skeleton className="hidden lg:flex h-12 w-32 mr-4"/> 
+                        <Skeleton className="hidden lg:flex h-12 w-72 "/> 
+                        
                     </div>
-                </Button>
-            </div>
+                </> 
+                :
+                <>
+                    <Carousel className="w-full md:overflow-auto relative">
+                        <CarouselPrevious variant="outline" className="absolute left-1 top-1/2 transform -translate-y-1/2 "/>    
+                        <div className="mx-12">
+                            <CarouselContent>
+                                {categories.map((category, index) => {
+                                    return <CarouselItem key={index} className="basis-1/10 mt-2">
+                                        <IconWithLabel type={String(category)}/>
+                                    </CarouselItem>
+                                })}
+                            </CarouselContent>
+                        </div>
+                        <CarouselNext variant="outline" className="absolute right-1 top-1/2 transform -translate-y-1/2 "/>
+                    </Carousel>            
+                    <div className="md:space-x-4 mt-2 lg:mt-0 hidden sm:flex">
+                        <Button variant={"outline"} className="rounded-lg">
+                            <RiSoundModuleLine className="rotate-90"/>
+                            <div className="text-mediumText font-sm py-2">
+                                Filter
+                            </div>
+                        </Button>
+
+                        <Button asChild variant={"outline"} className="rounded-lg" onClick={() => {
+                            setWithTax(!withTax)
+                        }}> 
+                            <div className="text-mediumText font-sm py-2">              
+                                Display total before taxes
+                                <Switch checked={withTax}/>
+                            </div>
+                        </Button>
+                    </div>
+                </>
+
+            }
+            
         </div>
     );
 }
